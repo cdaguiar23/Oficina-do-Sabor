a57960a5626bc3b3c11fe38d1a3c2dc70211ed3c
Claudionei de Aguiar <cdaguiar23@gmail.com>
2023-10-23 00:51:26 -0300

    Customizando botão voltar e atualizando nome do usuário

--- app/src/main/java/com/cdaguiar/instagram/activity/EditarPerfilActivity.java
+++ app/src/main/java/com/cdaguiar/instagram/activity/EditarPerfilActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cdaguiar.instagram.R;
 import com.cdaguiar.instagram.helper.UsuarioFirebase;
+import com.cdaguiar.instagram.model.Usuario;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -20,10 +23,14 @@ public class EditarPerfilActivity extends AppCompatActivity {
     private TextView textAlterarFoto;
     private TextInputEditText editNomePerfil, editEmailPerfil;
     private Button buttonSalavarAlteracoes;
+    private Usuario usuarioLoagado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_perfil);
+
+        // Confogurações iniciais
+        usuarioLoagado = UsuarioFirebase.getDadoUsuarioLogado();
 
         // Configurar toolbar
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
@@ -40,6 +47,22 @@ public class EditarPerfilActivity extends AppCompatActivity {
         FirebaseUser usuarioPerfil = UsuarioFirebase.getUsuarioAtual();
         editNomePerfil.setText(usuarioPerfil.getDisplayName());
         editEmailPerfil.setText(usuarioPerfil.getEmail());
+
+        // Salvar alterações do nome
+        buttonSalavarAlteracoes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nomeAtualizado = editNomePerfil.getText().toString();
+
+                // Atualizar nome do perfil
+                UsuarioFirebase.atualizarNomeUsuario(nomeAtualizado);
+
+                // Atualizar nome no banco de dados
+                usuarioLoagado.setNome(nomeAtualizado);
+                usuarioLoagado.atualizar();
+                Toast.makeText(EditarPerfilActivity.this, "Dados alterados com sucesso!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void inicializarComponentes() {
@@ -50,5 +73,11 @@ public class EditarPerfilActivity extends AppCompatActivity {
         buttonSalavarAlteracoes = findViewById(R.id.buttonSalvarAlteracoes);
         editEmailPerfil.setFocusable(false);
 
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return false;
     }
 }
