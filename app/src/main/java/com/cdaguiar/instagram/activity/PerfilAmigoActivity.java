12957913637776cf3a6f93f99f51a2d0391aa871
Claudionei de Aguiar <cdaguiar23@gmail.com>
2023-10-24 18:33:26 -0300

    Exibindo dados do perfil do amigo

--- app/src/main/java/com/cdaguiar/instagram/activity/PerfilAmigoActivity.java
+++ app/src/main/java/com/cdaguiar/instagram/activity/PerfilAmigoActivity.java
@@ -1,27 +1,46 @@
 package com.cdaguiar.instagram.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.cdaguiar.instagram.R;
+import com.cdaguiar.instagram.helper.ConfiguracaoFirebase;
 import com.cdaguiar.instagram.model.Usuario;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class PerfilAmigoActivity extends AppCompatActivity {
 
     private Usuario usuarioSelecionado;
     private Button buttonAcaoPerfil;
+    private CircleImageView imagePerfil;
+
+    private DatabaseReference usuarioRef;
+    private DatabaseReference usuarioAmigoRef;
+    private ValueEventListener valueEventListenerAmigo;
+    private TextView textPublicacoes, textSeguidores, textSeguindo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_amigo);
 
+        // Configurações inciiais
+        usuarioRef = ConfiguracaoFirebase.getFirebase().child("usuarios");
+
         // Inicializar componentes
-        buttonAcaoPerfil = findViewById(R.id.buttonAcaoPerfil);
-        buttonAcaoPerfil.setText("Seguir");
+        inicializarComponentes();
 
         // Configurar toolbar
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
@@ -38,12 +57,63 @@ public class PerfilAmigoActivity extends AppCompatActivity {
 
             // Configura nome do usuário na toolbar
             getSupportActionBar().setTitle(usuarioSelecionado.getNome());
+
+            // Recuperar foto do usuário
+            String caminhoFoto = usuarioSelecionado.getCaminhoFoto();
+            if (caminhoFoto != null) {
+                Uri url = Uri.parse(caminhoFoto);
+                Glide.with(PerfilAmigoActivity.this).load(url).into(imagePerfil);
+            }
         }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        recuperarDadosPerfilAmigo();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        usuarioAmigoRef.removeEventListener(valueEventListenerAmigo);
+    }
+
+    private void recuperarDadosPerfilAmigo() {
+        usuarioAmigoRef = usuarioRef.child(usuarioSelecionado.getId());
+        valueEventListenerAmigo = usuarioAmigoRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Usuario usuario = snapshot.getValue(Usuario.class);
+                String postagens = String.valueOf(usuario.getPostagens());
+                String seguindo = String.valueOf(usuario.getSeguindo());
+                String seguidores = String.valueOf(usuario.getSeguidores());
+
+                // Configura valores recuperadores
+                textPublicacoes.setText(postagens);
+                textSeguidores.setText(seguidores);
+                textSeguindo.setText(seguindo);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     @Override
     public boolean onSupportNavigateUp() {
         finish();
         return false;
+    }
+
+    private void inicializarComponentes() {
+        imagePerfil = findViewById(R.id.imageEditarPerfil);
+        buttonAcaoPerfil = findViewById(R.id.buttonAcaoPerfil);
+        textPublicacoes = findViewById(R.id.textPublicacoes);
+        textSeguidores = findViewById(R.id.textSeguidores);
+        textSeguindo = findViewById(R.id.textSeguindo);
+        buttonAcaoPerfil.setText("Seguir");
     }
 }
