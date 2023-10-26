209c206d2a211d80ae06d6723b5de29f81004eaf
Claudionei de Aguiar <cdaguiar23@gmail.com>
2023-10-25 23:10:17 -0300

    Criando interface para os filtros

--- app/src/main/java/com/cdaguiar/instagram/activity/FiltroActivity.java
+++ app/src/main/java/com/cdaguiar/instagram/activity/FiltroActivity.java
@@ -1,18 +1,27 @@
 package com.cdaguiar.instagram.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 import com.cdaguiar.instagram.R;
 
 public class FiltroActivity extends AppCompatActivity {
 
+    static {
+        System.loadLibrary("NativeImageProcessor");
+    }
     private ImageView imageFotoEscolhida;
     private Bitmap imagem;
+    private Bitmap imagemFiltro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +31,54 @@ public class FiltroActivity extends AppCompatActivity {
         // Inicializar componentes
         imageFotoEscolhida = findViewById(R.id.imageFotoEscolhida);
 
+        // Configurar toolbar
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle("Filtros");
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close_black_24dp);
+
         //Recupera a imagem escolhid apelo usuário
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
             byte[] dadosImagem = bundle.getByteArray("fotoEscolhida");
             imagem = BitmapFactory.decodeByteArray(dadosImagem, 0, dadosImagem.length);
-            imageFotoEscolhida.setImageBitmap(imagem);
+            Bitmap copyBitmap = imagem.copy(Bitmap.Config.ARGB_8888, true);
+            // ...
+        } else {
+            System.out.println("Bitmap é vazio");            imageFotoEscolhida.setImageBitmap(imagem);
+
+            if (imagem != null) {
+                // Faça as operações com o bitmap aqui
+
+            }
+            imagemFiltro.copy(imagem.getConfig(), true);
+//            Filter filter = FilterPack.getAdeleFilter(getApplicationContext());
+//            imageFotoEscolhida.setImageBitmap(filter.processFilter(imagemFiltro));
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_filtro, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.ic_salvar_postagem:
+
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return false;
     }
 }
