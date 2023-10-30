8c50c85e54c0c975ba366950587cb715baaf1e4b
Claudionei de Aguiar <cdaguiar23@gmail.com>
2023-10-30 18:37:24 -0300

    Recuperando comentários

--- app/src/main/java/com/cdaguiar/instagram/activity/ComentariosActivity.java
+++ app/src/main/java/com/cdaguiar/instagram/activity/ComentariosActivity.java
@@ -1,5 +1,6 @@
 package com.cdaguiar.instagram.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,17 +9,35 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.cdaguiar.instagram.R;
+import com.cdaguiar.instagram.adapter.AdapterComentario;
+import com.cdaguiar.instagram.adapter.AdapterFeed;
+import com.cdaguiar.instagram.helper.ConfiguracaoFirebase;
 import com.cdaguiar.instagram.helper.UsuarioFirebase;
 import com.cdaguiar.instagram.model.Comentario;
 import com.cdaguiar.instagram.model.Usuario;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ComentariosActivity extends AppCompatActivity {
 
     private EditText editComentario;
     private String idPostagem;
     private Usuario usuario;
+    private RecyclerView recyclerComentarios;
+    private AdapterComentario adapterComentario;
+    private List<Comentario> listaComentarios = new ArrayList<>();
+    private DatabaseReference firebaseRef;
+    private DatabaseReference comentariosRef;
+    private ValueEventListener valueEventListenerComentarios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +46,11 @@ public class ComentariosActivity extends AppCompatActivity {
 
         // Inicializa componentes
         editComentario = findViewById(R.id.editComentario);
+        recyclerComentarios = findViewById(R.id.recyclerComentarios);
 
         // Configurações inciais
         usuario = UsuarioFirebase.getDadoUsuarioLogado();
+        firebaseRef = ConfiguracaoFirebase.getFirebase();
 
         // Configura toolbar
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
@@ -39,12 +60,37 @@ public class ComentariosActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close_black_24dp);
 
+        // Configura recycler view
+        adapterComentario = new AdapterComentario(listaComentarios, getApplicationContext());
+        recyclerComentarios.setHasFixedSize(true);
+        recyclerComentarios.setLayoutManager(new LinearLayoutManager(this));
+        recyclerComentarios.setAdapter(adapterComentario);
+
         // Recuperar o id da postagem
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
             idPostagem = bundle.getString("idPostagem");
         }
 
+    }
+
+    private void recuperarComentarios() {
+        comentariosRef = firebaseRef.child("comentarios").child("idPostagem");
+        valueEventListenerComentarios = comentariosRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                listaComentarios.clear();
+                for (DataSnapshot ds : snapshot.getChildren()) {
+                    listaComentarios.add(ds.getValue(Comentario.class));
+                }
+                adapterComentario.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     public void salvarComentario(View view) {
@@ -72,5 +118,17 @@ public class ComentariosActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         finish();
         return false;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        recuperarComentarios();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        comentariosRef.removeEventListener(valueEventListenerComentarios);
     }
 }
