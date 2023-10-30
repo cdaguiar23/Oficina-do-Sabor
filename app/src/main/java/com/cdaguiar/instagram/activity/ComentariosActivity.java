c6c35fe16d57635d7b2a172979c88701dbbee998
Claudionei de Aguiar <cdaguiar23@gmail.com>
2023-10-30 17:00:16 -0300

    Desenvolvimento de salvar comentários

--- app/src/main/java/com/cdaguiar/instagram/activity/ComentariosActivity.java
+++ app/src/main/java/com/cdaguiar/instagram/activity/ComentariosActivity.java
@@ -3,16 +3,33 @@ package com.cdaguiar.instagram.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.widget.Toolbar;
 
 import com.cdaguiar.instagram.R;
+import com.cdaguiar.instagram.helper.UsuarioFirebase;
+import com.cdaguiar.instagram.model.Comentario;
+import com.cdaguiar.instagram.model.Usuario;
 
 public class ComentariosActivity extends AppCompatActivity {
+
+    private EditText editComentario;
+    private String idPostagem;
+    private Usuario usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comentarios);
+
+        // Inicializa componentes
+        editComentario = findViewById(R.id.editComentario);
+
+        // Configurações inciais
+        usuario = UsuarioFirebase.getDadoUsuarioLogado();
 
         // Configura toolbar
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
@@ -22,5 +39,38 @@ public class ComentariosActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close_black_24dp);
 
+        // Recuperar o id da postagem
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            idPostagem = bundle.getString("idPostagem");
+        }
+
+    }
+
+    public void salvarComentario(View view) {
+        String textoComentário = editComentario.getText().toString();
+        if (textoComentário != null && !textoComentário.equals("")) {
+            Comentario comentario = new Comentario();
+            comentario.setIdPostagem(idPostagem);
+            comentario.setIdUsuario(usuario.getId());
+            comentario.setNomeUsuario(usuario.getNome());
+            comentario.setCaminhoFoto(usuario.getCaminhoFoto());
+            comentario.setComentario(textoComentário);
+            if (comentario.salvar()) {
+                Toast.makeText(this, "Comentário salvo com sucesso!!", Toast.LENGTH_SHORT).show();
+            }
+
+        } else {
+            Toast.makeText(this, "Insira o comentário antes de salvar!", Toast.LENGTH_SHORT).show();
+        }
+
+        // Limpar o comentário digitado
+        editComentario.setText("");
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return false;
     }
 }
